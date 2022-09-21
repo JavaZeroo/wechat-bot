b4944c290ffb90fd47a7b2e00dd63b013e53a706
JavaZero <2487163254@qq.com>
2022-09-21 15:16:16 +0800

    update

--- WeChatBot.py
+++ WeChatBot.py
@@ -9,14 +9,59 @@ NOW = datetime.datetime.now()
 
 class WeChatBot():
 
-    def __init__(self, corpid, corpsecret) -> None:
+    def __init__(self, agentid, corpid, corpsecret) -> None:
         """
         """
+        self.agentid = agentid
         self.corpid = corpid
         self.corpsecret = corpsecret
+        self.proxies = None
         self._checkconfig()
         pass
    
+
+
+    def set_proxies(self, proxies):
+        self.proxies = proxies
+        try:
+            res = requests.get('http://ip-api.com/json', timeout=10, proxies=self.proxies).json()
+            print("Using ip %s" % res['query'])
+        except Exception as e:
+            print("Proxies may not be available")
+            print(e)
+
+
+    def send_massage(self, title, description, touser='@all', toparty='@all', totag='@all', url="blog.jimmyisme.top"):
+        body = {
+            "touser" : touser,
+            "toparty" : toparty,
+            "totag" : totag,
+            "msgtype" : "textcard",
+            "agentid" : self.agentid,
+            "textcard" : {
+                        "title" : title,
+                        "description" : description,
+                        "url" : url,
+            },
+            "enable_id_trans": 0,
+            "enable_duplicate_check": 0,
+            "duplicate_check_interval": 1800
+        }
+        # print(body)
+        post_url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={self.access_token}"
+        try:
+            res = requests.post(post_url, json=body,timeout=10, proxies=self.proxies).json()
+        except requests.RequestException as e:
+            raise Exception(f"Cannot post message: {e}")
+        error_code = res['errcode']
+        if error_code == 0:
+            print("Succeful send message")
+            pass    
+        else:
+            check_error = f"https://open.work.weixin.qq.com/devtool/query?e={error_code}"
+            raise Exception(f"Return with Error Code {error_code}\nPlease Check in {check_error}")
+
+
     def _checkconfig(self):
 
         if os.path.exists(config_dir):
@@ -32,11 +77,13 @@ class WeChatBot():
             self.access_token = self._get_access_token()
         self._create_config()
 
+
     def _create_config(self):
         data = {'access_token': {'key': self.access_token, 'expired_at': (NOW + datetime.timedelta(seconds=7200)).strftime("%Y-%m-%d %H:%M:%S")}}
         with open(config_dir, 'w') as f:
             yaml.dump(data, f)
         pass
+
 
     def _is_expired(self, data):
         expire_at = data['access_token']['expired_at']
@@ -50,10 +97,11 @@ class WeChatBot():
             print('Expired')
             return True
 
+
     def _get_access_token(self):
         url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.corpid}&corpsecret={self.corpsecret}"
         try:
-            res = requests.get(url, timeout=10).json()
+            res = requests.get(url, timeout=10, proxies=self.proxies).json()
         except requests.exceptions.RequestException as e:
             raise Exception(f"Cannot get access_token: {e}")
 
@@ -66,39 +114,11 @@ class WeChatBot():
             check_error = f"https://open.work.weixin.qq.com/devtool/query?e={error_code}"
             raise Exception(f"Return with Error Code {error_code}\nPlease Check in {check_error}")
 
-    def send_massage(self, agentid, title, description, touser='@all', toparty='@all', totag='@all', url="blog.jimmyisme.top"):
-        body = {
-            "touser" : touser,
-            "toparty" : toparty,
-            "totag" : totag,
-            "msgtype" : "textcard",
-            "agentid" : agentid,
-            "textcard" : {
-                        "title" : title,
-                        "description" : description,
-                        "url" : url,
-            },
-            "enable_id_trans": 0,
-            "enable_duplicate_check": 0,
-            "duplicate_check_interval": 1800
-        }
-        print(body)
-        post_url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={self.access_token}"
-        try:
-            res = requests.post(post_url, json=body,timeout=10).json()
-        except requests.RequestException as e:
-            raise Exception(f"Cannot post message: {e}")
-        error_code = res['errcode']
-        if error_code == 0:
-            print("Succeful send message")
-            pass    
-        else:
-            check_error = f"https://open.work.weixin.qq.com/devtool/query?e={error_code}"
-            raise Exception(f"Return with Error Code {error_code}\nPlease Check in {check_error}")
 
 if __name__=="__main__":
     corpid = 'wwf210418e935eb1d1'
     corpsecret = '-wCwLjdsNM-Gwao6l4m-7jta2_Ecerq-5N76E8IBJ3o'
 
     bot = WeChatBot(corpid, corpsecret)
+
 
--- main.py
+++ main.py
@@ -1,51 +1,65 @@
-import requests
-import yaml
-import os
+###
+# 火车票  https://www.free-api.com/use/520
+# 土味 https://api.lovelive.tools/api/SweetNothings
+# https://www.free-api.com/doc/374
+# 农历 https://www.free-api.com/use/547
+# 假期 http://timor.tech/api/holiday/
+###
+
+import logging
+logging.basicConfig(filename='running.log', encoding='utf-8', level=logging.DEBUG)
 from WeChatBot import WeChatBot
+from easydict import EasyDict as edict
 import datetime
+import requests
 NOW = datetime.datetime.now()
-
-
-# 发送微信应用消息
-# def sendWxMsg(title,desc):
-#    # 从redis获取token
-#    access_token = cache.get('wx_access_token')
-#    sendbody={
-#       "touser": touser,
-#       "toparty": toparty,
-#       "totag": totag,
-#       "msgtype": "textcard",
-#       "agentid": agentid,
-#       "textcard" : {"title" : title,"description" : f"\n<div class=\"gray\">{desc}</div>","url" : "www.baidu.com","btntxt":"更多"
-#          },
-#       "safe": 0,
-#       "enable_id_trans": 0,
-#       "enable_duplicate_check": 0
-#    }
-#    try:
-#       res = requests.post(f"{wxurl}/cgi-bin/message/send?access_token={access_token}", json=sendbody,timeout=10)
-#       print(f"发送结果：{res.json()}")
-#    except requests.exceptions.RequestException as e:
-#       print(e)
-#       return "发送异常！"
-#    if res.json()['errcode'] != 0:
-#     print('access_token可能失效或错误，重新获取')
-#       access_token = getWxToken()
-#       res = requests.post(f"{wxurl}/cgi-bin/message/send?access_token={access_token}", json=sendbody,timeout=10)
-#       print(f"发送结果：{res.json()}")
-#    return res.text
+LOCATION = '101280105'
+KEY = 'e67b0a19d4d14bb4a525ed5cd686c020'
 
 def main():
    corpid = 'wwf210418e935eb1d1'
    corpsecret = '-wCwLjdsNM-Gwao6l4m-7jta2_Ecerq-5N76E8IBJ3o'
    agentid = 1000002
-
-   bot = WeChatBot(corpid, corpsecret)
+   bot = WeChatBot(agentid, corpid, corpsecret)
    title = f"今天是{NOW.strftime('%Y-%m-%d')}"
-   description = f"测试消息\n<b>测试html</b>"
-   bot.send_massage(agentid=agentid, title=title, description=description)
+   description = f"测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>测试消息\n<b>测试html</b>"
+   proxies={
+      'http':'socks5://110.40.190.64:2016', 
+      'https':'socks5://110.40.190.64:2016'
+   }
+   weather = get_weather(LOCATION, KEY)
+   print(weather)
+   bot.set_proxies(proxies)
+   # bot.send_massage(title=title, description=weather)
    pass
 
+# 待处理返回值
+def get_weather(location, key):
+   # weather = edict(requests.get(f'https://devapi.qweather.com/v7/weather/now?location={location}&key={key}').json())
+   weather = edict(requests.get(f'https://api.vvhan.com/api/weather?city=花都').json()).info
+   print(weather)
+   type = weather.type
+   high = weather.high
+   low = weather.low
+   tip = weather.tip
+   return f'今天{type} 最{high}最{low}\n{tip}'
+
+
+# 未完成
+def get_holiday():
+   try:
+      holiday = edict(requests.get(f'http://timor.tech/api/holiday/next/').json())
+      logging.debug('获取节日成功')
+   except:
+      logging.warning('获取节日失败！！')
+      return None
+   
+   return holiday
+
+# 待处理返回值
+def get_trainTicket(depart, dest, time=NOW):
+   url = f'http://huoche.tuniu.com/yii.php?r=train/trainTicket/getTickets&primary[departureDate]={time}&primary[departureCityName]={depart}&primary[arrivalCityName]={dest}'
+   trainTicket = edict(requests.get(url).json())
 
 if __name__=="__main__":
    main()
